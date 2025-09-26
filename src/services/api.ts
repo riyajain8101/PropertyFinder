45cbe3560fabb536a7ffa920c30bd1a96be2ebef
riyajain8101 <riya.jain8101@gmail.com>
2025-09-26 03:08:47 -0700

    Updated api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:3001/api';
+/*const API_BASE_URL = 'http://localhost:3001/api';*/
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001/api";
 
 class ApiError extends Error {
   constructor(public status: number, message: string) {
