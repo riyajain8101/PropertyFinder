83e8569415b95210f343e0701eb2fd5aefd40234
riyajain8101 <riya.jain8101@gmail.com>
2025-09-25 19:11:22 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/*
+  export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+});
+*/
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001',
+    },
   },
 });
